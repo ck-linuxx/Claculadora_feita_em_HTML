244bdb2983cdafab2eb742bb74fff17ef6568dc0
Clark <amorimclark@gmail.com>
2021-08-01 12:52:37 -0300

    master

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ function insert(num){
 function cls(){
     document.getElementById('resultado').innerHTML = ''
 }
+
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML 
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1)
+}
