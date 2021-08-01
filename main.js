75d5bd617cf99a8f79d9c6f565ce36bf14916787
Clark <amorimclark@gmail.com>
2021-08-01 12:58:35 -0300

    master

--- main.js
+++ main.js
@@ -17,4 +17,7 @@ function calcular(){
     if(resultado ){
         document.getElementById('resultado').innerHTML = eval(resultado)
     }
+    else{
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
 }
