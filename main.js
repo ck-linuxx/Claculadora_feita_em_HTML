4bd1eed8b3a657506d7bfb969a365fec91007106
Clark <amorimclark@gmail.com>
2021-07-31 21:44:56 -0300

    master

---
+++ main.js
@@ -0,0 +1,8 @@
+function insert(num){
+     var numero =document.getElementById('resultado').innerHTML 
+     document.getElementById('resultado').innerHTML = numero + num
+}
+
+function cls(){
+    document.getElementById('resultado').innerHTML = ''
+}
